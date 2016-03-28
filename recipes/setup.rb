2ffb98a281250411f862e49a3c23f3492f2c0eea
Deepak Aggarwal <deepak@Deepaks-MacBook-Air.local>
2016-03-28 09:07:41 +0530

    Removed tagging of instances

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,7 +3,6 @@ extra_vars = {}
 extra_vars['opsworks'] = node['opsworks']
 extra_vars['ansible']  = node['ansible']
 
-Chef::Application.fatal!("'ansible['environment']' must be defined in custom json for the opsworks stack") if node['ansible'].nil? || node['ansible']['environment'].nil? || node['ansible']['environment'].empty?
 Chef::Application.fatal!("'ansible['playbooks']' must be defined in custom json for the opsworks stack") if node['ansible'].nil? || node['ansible']['playbooks'].nil? || node['ansible']['playbooks'].empty?
 Chef::Application.fatal!("'ansible['folder']' must be defined in custom json for the opsworks stack") if node['ansible'].nil? || node['ansible']['folder'].nil? || node['ansible']['folder'].empty?
 
@@ -41,18 +40,6 @@ end
 execute 'extract_some_tar' do
   command 'unzip /etc/opsworks-customs/ansible.zip'
   cwd zippath
-end
-
-# If the role for this layer is defined in custom json then set the role to what's defined
-# If not, set the role to the name of the layer
-role = node['ansible']['layers'][layer]['role'] rescue nil
-if role.nil?
-  role = layer
-end
-
-execute "tag instance" do
-  command "aws ec2 create-tags --tags Key=environment,Value=#{environment} Key=role,Value=#{role} --resources `curl http://169.254.169.254/latest/meta-data/instance-id/` --region #{node['opsworks']['instance']['region']}"
-  action :run
 end
 
 execute "setup" do
